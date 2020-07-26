131f70e33b33fedd47abff025fe3930baac833d8
Sanjay Bhargav Bandaru <sanjay.bandaru91@gmail.com>
2020-07-26 23:32:48 +0530

    ok3

--- Server/server.js
+++ Server/server.js
@@ -12,6 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(apiRoutes);
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.listen(PORT, () => {
 	console.log("Server is running at port", PORT);
